d5d3bd6cc8a0183e095e75f149fd5bf10d7c5d96
bytedrex15-beep <bytedrex15@gmail.com>
2026-03-11 10:20:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['255743140476','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Byte0XFF', 'Professor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
     botName: 'Knight Bot Mini',
     prefix: '.',
     sessionName: 'session',
-    sessionID: process.env.SESSION_ID || '',
+    sessionID: process.env.SESSION_ID || 'KnightBot!H4sIAAAAAAAAA5VUyZKjOBD9F11xtNnBjqiIAWy84t14mZiDDALLxgIkYcAd/vcJXF1dfZjpqbkJicx8+d7L/A5IihmaoBp0v4OM4jvkqDnyOkOgC+wiihAFLRBCDkEXTBYPC/qOKVSl5yB6IXu/GBzc3NgJ+74fCVLmTbPjLJCU+A08WyArTgkOfpMwDUeBANX67kkrYxQMjB3XbTX28uFcI1Ynt0JhokSXfO9ab+DZZISYYhL3szO6IQqTCaoXENOvwZenUej3zJ2g67uqXeTtNvGUg2jH/Aq5tRMn25yIKTowMvoafEoy3/Q8OxwfTIMoG7Mf1II/iW7jGKWyOXcCmvPzMofl9h0+wzFB4ShEhGNef5l31ZsGj2RYGGURIkXuBG3tEeeWE+rXbdJ27nm+ELQ5sYi0/RrwSgsfa1MdloT2sVcOa9mx1jQSrrRqb3p5OtX2NmmPd4+h9yvwBf3wyvX/8L72lmaV9TxhdNZMZb9xhFHUmS5WJiu5lLuGhIb3gh60ze3wNfgLVK16y7UklVnHVzExq/0RCv1prO1Z6B/6G6f0Q7w93PvBJ3zIC/o7lCh3/WN9v5aKa/P6uiUpFGXdFgd2gB7bxb5TmGR50cXK7O+CpdNG4+LiF+nYVe/TMo7tjnw0QynSSFnOQnaZq6o5vSzfXh1dUT0KQVd6tgBFMWacQo5T0tzJktYCMLyvUUARf9ELHqceZX0yeuxOtaYW+n2/Ps5WvBoc5fn4YCsslblck8VJst5AC2Q0DRBjKBxixlNae4gxGCMGun/+1QIEVfxduKacIrVAhCnjW1JkSQrDD1U/HmEQpAXh65oETnNAFHTFz2vEOSYxa3gsCKTBGd+Rc4acgW4EE4Z+dogoCkGX0wL9nFonDRvi95KhumrfBS1wewmCw2Y4Nc1QFUkVVUPvauYf7FvZpIVZ9o0gDlqAwNtLtpojsXKb6OQ9UDQ0xdAlU1REXZeb2Obh+RNzUyJEHOKEgS5wFs6mXFwH/QXmlScOBr1lbDmxBT57/DDLuxhH33bb6axT6BO/EtoXp3ZVifTn+5UvjDvFgLgze7mdt/Ute/uHJKALTm5+YV5fzIi+IyYcY+Tg2EWmcHQvUGaX4jA0loyFXjxJFGVA83jj3Lw6RDRzFLpaqf1bpYhiYewO6WQ7Ga2oeHOst6ZaiO44QL8Wm/a2Lqz28SZeuqUrwwvSdm2miJdOPLKvRbleEGEwTaaRIN6jLA9deWxbQyVZKwmE6JiLk+suF6/XvRTavd1pENGZja13G7/GKPmxvvDLYI16zWeE0Wsb/FDpP9V8B96YTny2fsnxY7/8y4zaO3+5ivRedCpwEkR3tWRnNDtWvUT0Ixydzlm1WW7d3l1IpuD5/KsFsgTyKKU30AXsdoKgBWhaNBYekSj9TSXHGo16y3jUtJ1Axq3PsdjgG2Ic3jLQlQxDkSVFVfT3vxY0zYaQnRsGBqp1K8Dzb6uLaZhkBwAA',
     newsletterJid: '120363161513685998@newsletter', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
@@ -64,7 +64,7 @@ module.exports = {
     messages: {
       wait: '⏳ Please wait...',
       success: '✅ Success!',
-      error: '❌ Error occurred!',
+      error: ' Error occurred!',
       ownerOnly: '👑 This command is only for bot owner!',
       adminOnly: '🛡️ This command is only for group admins!',
       groupOnly: '👥 This command can only be used in groups!',
