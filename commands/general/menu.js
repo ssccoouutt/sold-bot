63b5b1465a718c8c24e1407c1e7b96bd5a63b823
ssccoouutt <144871295+ssccoouutt@users.noreply.github.com>
2026-03-30 09:22:31 +0500

    Update menu.js

--- commands/general/menu.js
+++ commands/general/menu.js
@@ -166,7 +166,7 @@ module.exports = {
             forwardingScore: 1,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-              newsletterJid: config.newsletterJid || '120363161513685998@newsletter',
+              newsletterJid: config.newsletterJid || '120363304414452603@newsletter',
               newsletterName: config.botName,
               serverMessageId: -1
             }
